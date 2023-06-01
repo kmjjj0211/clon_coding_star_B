9b36977f7aad9e474b1af09f797336de8df3d65f
kmjjj0211 <kkmj9702@naver.com>
2023-06-02 01:12:53 +0900

    header_complete

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// gnb - subMunu
+let gnbTitle = document.querySelectorAll(".gnb>ul>li>a");
+let subNavLists = document.querySelectorAll(".sub_nav_lists");
+let sub_nav_wrap = document.querySelector(".sub_nav_wrap");
+
+for(let i=0; i<gnbTitle.length; i++){
+    gnbTitle[i].addEventListener("mouseover",e=>{
+        for(let j=0; j<subNavLists.length; j++){
+            subNavLists[j].classList.remove("show");  
+            gnbTitle[j].classList.remove("mouseover");
+        }
+        gnbTitle[i].classList.add("mouseover");
+        subNavLists[i].classList.add("show");
+        sub_nav_wrap.style.padding = "0px 0px 30px 0px";
+    })
+}
+
+for(let k=0; k<subNavLists.length; k++){
+    subNavLists[k].addEventListener("mouseover",e=>{
+        subNavLists[k].classList.add("show");
+        gnbTitle[k].classList.add("mouseover");
+        sub_nav_wrap.style.padding = "0px 0px 30px 0px";
+    })
+}
+
+for(let k=0; k<subNavLists.length; k++){
+    subNavLists[k].addEventListener("mouseout",e=>{
+        subNavLists[k].classList.remove("show");
+        sub_nav_wrap.style.padding = "0px";
+        gnbTitle[k].classList.remove("mouseover");
+    })
+}
